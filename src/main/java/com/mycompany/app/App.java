5a9b9101f060f03ce42ed0e4684cb9cde8a2fc0e
rajarshi.mukherjee <rajarshi.mukherjee@BHUKRK259672L.ad.infosys.com>
2020-10-07 20:40:21 +0530

    vulnerabilities add

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,11 @@
 package com.mycompany.app;
 
+import java.io.Console;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Hello world!
  */
@@ -15,6 +21,23 @@ public class App
     }
 
     private final String getMessage() {
+    	Properties prop = new Properties(); 
+    	try {
+			prop.load(new FileInputStream("config.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+    	String password = prop.getProperty("password");
+    	System.out.println(password);
+    	
+    	try {
+    		throw new Exception();
+    	}
+    	catch(Exception e) {
+    		System.console().writer().println(e);
+    	}
         return message;
     }
 
